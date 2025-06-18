323a4aeeda2b85d43521e92fbd8f44dbee8fb5fd
kollors <kollors@mail.ru>
2025-06-18 10:53:12 +0300

    фикс фетчера

--- dist/graphql-fetcher.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @param {string} query GraphQL строка.
- * @param {object} variables Параметры для запроса.
- * @returns {Function} Функция для создания запроса.
- */
-export declare function graphqlFetcher<TData, TVariables>(query: string, variables?: TVariables): () => Promise<TData>;
